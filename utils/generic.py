30bb5aef73134051dfc746d37582f231bc7fa4ac
Francesco Picetti <francesco.picetti@polimi.it>
2020-11-16 11:55:53 +0100

    Added read_args, write_args

--- utils/generic.py
+++ utils/generic.py
@@ -1,18 +1,22 @@
+from typing import Dict, List, Optional, Union
+from pathlib import Path
 from math import floor, log10
 import random
 import string
+import json
+from argparse import Namespace
 
 
-def random_code(n=6):
+def random_code(n: int = 6) -> str:
     return ''.join([random.choice(string.ascii_letters + string.digits)
                     for _ in range(int(n))])
 
 
-def ten_digit(number):
+def ten_digit(number: float) -> int:
     return int(floor(log10(number)) + 1)
 
 
-def sec2time(seconds):
+def sec2time(seconds: float) -> str:
     s = seconds % 60
     m = (seconds // 60) % 60
     h = seconds // 3600
@@ -20,8 +24,22 @@ def sec2time(seconds):
     return timestamp
 
 
+def read_args(filename: Union[str, Path]) -> Namespace:
+    args = Namespace()
+    with open(filename, 'r') as fp:
+        args.__dict__.update(json.load(fp))
+    return args
+
+
+def write_args(filename: Union[str, Path], args: Namespace, indent: int = 2) -> None:
+    with open(filename, 'w') as fp:
+        json.dump(args.__dict__, fp, indent=indent)
+
+
 __all__ = [
     "random_code",
     "ten_digit",
-    "sec2time"
+    "sec2time",
+    "read_args",
+    "write_args",
 ]
